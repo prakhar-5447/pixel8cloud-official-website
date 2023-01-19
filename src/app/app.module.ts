0a9b8b6746f15cdfd35464540ace422b8954ac27
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-01-19 20:07:53 +0530

    basic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavModalComponent } from './modal/nav-modal/nav-modal.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, NavModalComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
