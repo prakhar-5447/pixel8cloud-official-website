b2a811d7adbffc54d0c764b8f1cf07e4a32ac103
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-01-19 20:22:21 +0530

    create footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavModalComponent } from './modal/nav-modal/nav-modal.component';
 
 import { MatDialogModule } from '@angular/material/dialog';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NavModalComponent],
+  declarations: [AppComponent, NavbarComponent, NavModalComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
